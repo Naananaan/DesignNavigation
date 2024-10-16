5a447b94e4e1e781bfb4bc9a261339e35d7aa0eb
Jaana Ahola <jaanaaholaa@gmail.com>
2024-10-16 12:53:21 +0300

    Lisätty DesignNavigation-tiedostot

--- App.js
+++ App.js
@@ -11,15 +11,17 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        screenOptions={({ navigation, route }) => ({
-          header: (props) => <AppBar {...props} navigation={navigation} />,
-        })}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Second" component={SecondScreen} />
-        <Stack.Screen name="Third" component={ThirdScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      screenOptions={({ navigation, route }) => ({
+        header: ({ back }) => (
+          <AppBar navigation={navigation} back={back} route={route} />
+        ),
+      })}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Second" component={SecondScreen} />
+      <Stack.Screen name="Third" component={ThirdScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
--- AppBar.js
+++ AppBar.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
-export default function AppBar({ navigation }) {
+import {Appbar } from 'react-native-paper';
+const AppBar = ({ navigation, back, route }) => {
   return (
-    <View style={styles.container}>
-      <Button title="Home" onPress={() => navigation.navigate('Home')} />
-      <Text style={styles.title}>App Title</Text>
-    </View>
+    <Appbar.Header>
+      {back ? (
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+      ) : null}
+      <Appbar.Content title={route.name} />
+    </Appbar.Header>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 10,
-    backgroundColor: '#f8f8f8',
-  },
-
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
+export default AppBar;
